db1d459c7a859fff00838643248b09f1b0a23a0f
konovnina <ea.knvn@gmail.com>
2021-08-02 12:15:44 +0400

    Add lesson03-hard

--- script.js
+++ script.js
@@ -11,3 +11,43 @@ console.log(mult);
 let powResult = mult ** 3;
 
 console.log(powResult.toString().substring(0, 2));
+
+//Усложненное задание к уроку 3
+//Часть 1. Переменная lang может принимать 2 значения: 'ru' 'en'. Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке
+let lang;
+while ((lang !== 'ru') && (lang !== 'en')) {
+  lang = prompt('Выберите язык (ru/en):', 'en');
+}
+let days = {
+  en: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday',
+  ru: 'Понедельник, Вторник, Среда, четверг, Пятница, Суббота, Воскресенье'
+};
+
+//a - через if
+if (lang === 'en') {
+  console.log(days.en);
+} else {
+  console.log(days.ru);
+}
+
+//b - через switch-case
+switch (lang) {
+  case 'en':
+    console.log(days.en);
+    break;
+  case 'ru':
+    console.log(days.ru);
+    break;
+  default:
+    console.log('Ошибка');
+}
+
+//c - поиск и вывод данных через 2-мерный массив
+let daysArray = [['en', 'ru'], [days.en, days.ru]];
+console.log(daysArray[1][daysArray[0].indexOf(lang)]);
+
+//Часть 2. У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Максим” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
+let namePerson = prompt('Введите ваше имя:', 'Артем');
+(namePerson === 'Артем') ? console.log('Директор') :
+  (namePerson === 'Максим') ? console.log('Преподаватель') :
+  console.log('Студент');
