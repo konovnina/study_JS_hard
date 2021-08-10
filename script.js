669e71fb0c77fa3f5e0a74c0e91e183fd9abe02b
konovnina <ea.knvn@gmail.com>
2021-08-10 10:41:44 +0400

    Update lesson09-hard

--- script.js
+++ script.js
@@ -3,46 +3,27 @@
 //Усложненное задание к уроку 9
 
 //элементы на странице
-let dateA = document.querySelector('.date__a');
-let dateB = document.querySelector('.date__b');
+const dateA = document.querySelector('.date__a');
+const dateB = document.querySelector('.date__b');
 
 //массивы данных
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 const month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+const hours = ['час', 'часа', 'часов'];
+const minutes = ['минута', 'минуты', 'минут'];
+const seconds = ['секунда', 'секунды', 'секунд'];
 
 //склонение слов
-const declension = function (num, tag) {
+const getWord = function (num) {
   let temp = num.toString();
   let lastDigit = parseInt(temp[temp.length - 1]);
 
-  if (tag === 'h') {
-    if ((lastDigit >= 2) && (lastDigit <= 4)) {
-      return num + ' часа';
-    } else if ((lastDigit === 1) && (num !== 11)) {
-      return num + ' час';
-    } else {
-      return num + ' часов';
-    }
-  }
-
-  if (tag === 'm') {
-    if ((lastDigit >= 2) && (lastDigit <= 4)) {
-      return num + ' минуты';
-    } else if ((lastDigit === 1) && (num !== 11)) {
-      return num + ' минута';
-    } else {
-      return num + ' минут';
-    }
-  }
-
-  if (tag === 's') {
-    if ((lastDigit >= 2) && (lastDigit <= 4)) {
-      return num + ' секунды';
-    } else if ((lastDigit === 1) && (num !== 11)) {
-      return num + ' секунда';
-    } else {
-      return num + ' секунд';
-    }
+  if ((lastDigit === 1) && (num !== 11)) {
+    return 0;
+  } else if ((lastDigit >= 2) && (lastDigit <= 4)) {
+    return 1;
+  } else {
+    return 2;
   }
 
 };
@@ -60,20 +41,20 @@ const outputDateA = function (date) {
   let weekDay = 'Сегодня ' + week[date.getDay() - 1] + ', ';
   let monthDay = date.getDate() + ' ' + month[date.getMonth() - 1] + ' ';
   let year = date.getFullYear() + ' года, ';  
-  let hours = declension(date.getHours(), 'h') + ' ';
-  let minutes = declension(date.getMinutes(), 'm') + ' ';
-  let seconds = declension(date.getSeconds(), 's') + ' ';
+  let hour = date.getHours() + ' ' + hours[getWord(date.getHours())] + ' ';
+  let minute = date.getMinutes() + ' ' + minutes[getWord(date.getMinutes())] + ' ';
+  let second = date.getSeconds() + ' ' + seconds[getWord(date.getSeconds())] + ' ';
 
-  return weekDay + monthDay + year + hours + minutes + seconds;
+  return weekDay + monthDay + year + hour + minute + second;
 };
 
-let outputDateB = function (date) {
+const outputDateB = function (date) {
   let day = addDigit(date.getDate()) + '.' + addDigit(date.getMonth()) + '.' + date.getFullYear();
   let time = addDigit(date.getHours()) + ':' + addDigit(date.getMinutes()) + ':' + addDigit(date.getSeconds());
   return day + ' — ' + time;
 };
 
-let start = function (){
+const start = function (){
   setInterval(function () {
     let date = new Date();
     dateA.textContent = outputDateA(date);
