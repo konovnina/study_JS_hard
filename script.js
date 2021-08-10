0232fda1f5b0b29305001a48ee778f57d57c9694
konovnina <ea.knvn@gmail.com>
2021-08-10 09:44:18 +0400

    Add lesson09-hard

--- script.js
+++ script.js
@@ -1,24 +1,84 @@
 'use strict';
 
-//Усложненное задание к уроку 7
-let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']; //создаем массив
+//Усложненное задание к уроку 9
 
-let date = new Date(); //Создаем объект даты
-let today = week[date.getDay() - 1];//Узнаем порядковый номер текущего дня недели
+//элементы на странице
+let dateA = document.querySelector('.date__a');
+let dateB = document.querySelector('.date__b');
 
-let weekList = document.createElement('ul');
-document.body.prepend(weekList);//Добавляем список в начало документа
-for (let i of week) {
-  let li = document.createElement('li');//Создаем элемент списка...
-  li.textContent = i;//...и добавляем в него текст
+//массивы данных
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
-  //Добавляем стили к определенным элементам
-  if ((li.textContent === 'Saturday') || (li.textContent === 'Sunday')) { //Суббота или воскресенье?
-    li.style.fontStyle = 'italic';
+//склонение слов
+const declension = function (num, tag) {
+  let temp = num.toString();
+  let lastDigit = parseInt(temp[temp.length - 1]);
+
+  if (tag === 'h') {
+    if ((lastDigit >= 2) && (lastDigit <= 4)) {
+      return num + ' часа';
+    } else if ((lastDigit === 1) && (num !== 11)) {
+      return num + ' час';
+    } else {
+      return num + ' часов';
+    }
   }
-  if (li.textContent === today) { //Текущий день недели?
-    li.style.fontWeight = 'bold';
+
+  if (tag === 'm') {
+    if ((lastDigit >= 2) && (lastDigit <= 4)) {
+      return num + ' минуты';
+    } else if ((lastDigit === 1) && (num !== 11)) {
+      return num + ' минута';
+    } else {
+      return num + ' минут';
+    }
   }
 
-  weekList.append(li);//Добавляем готовый элемент в конец списка
-}
+  if (tag === 's') {
+    if ((lastDigit >= 2) && (lastDigit <= 4)) {
+      return num + ' секунды';
+    } else if ((lastDigit === 1) && (num !== 11)) {
+      return num + ' секунда';
+    } else {
+      return num + ' секунд';
+    }
+  }
+
+};
+ 
+//подставить 0 в начало
+const addDigit = function (num) {
+  if (num.toString().length === 1) {
+    return '0' + num.toString();
+  } else {
+    return num;
+  }
+};
+
+const outputDateA = function (date) {
+  let weekDay = 'Сегодня ' + week[date.getDay() - 1] + ', ';
+  let monthDay = date.getDate() + ' ' + month[date.getMonth() - 1] + ' ';
+  let year = date.getFullYear() + ' года, ';  
+  let hours = declension(date.getHours(), 'h') + ' ';
+  let minutes = declension(date.getMinutes(), 'm') + ' ';
+  let seconds = declension(date.getSeconds(), 's') + ' ';
+
+  return weekDay + monthDay + year + hours + minutes + seconds;
+};
+
+let outputDateB = function (date) {
+  let day = addDigit(date.getDate()) + '.' + addDigit(date.getMonth()) + '.' + date.getFullYear();
+  let time = addDigit(date.getHours()) + ':' + addDigit(date.getMinutes()) + ':' + addDigit(date.getSeconds());
+  return day + ' — ' + time;
+};
+
+let start = function (){
+  setInterval(function () {
+    let date = new Date();
+    dateA.textContent = outputDateA(date);
+    dateB.textContent = outputDateB(date);
+  }, 1000);
+};
+
+start();
